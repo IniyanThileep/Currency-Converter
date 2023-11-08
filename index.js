4dc5ac63001c4c652f81e691db16c1f02fe1d433
IniyanThil <111782904+IniyanThileep@users.noreply.github.com>
2023-11-08 08:56:10 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const fromCur = document.querySelector(".from select");
+const toCur = document.querySelector(".to select");
+const getBtn = document.querySelector("from button");
+const exIcon = document.querySelector("from .reverse");
+const amount = document.querySelector("from input");
+const exRateTxt = document.querySelector("from .result");
